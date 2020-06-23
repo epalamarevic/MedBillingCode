d5f66d9d1a0a73e25abb609f25c9fa6dc860cf48
Emina Palamarevic <minapala342@gmail.com>
2020-06-22 21:04:04 -0400

    DiagnosisServices

---
+++ MedicalBilling.Models/DiagnosisModels/DiagnosisDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalBilling.Models.DiagnosisModels
+{
+    public class DiagnosisDetail
+    {
+        public int DiagnosisId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
